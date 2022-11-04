186c4f610a02b566e41445e9f4d473f11df227b8
Rennan Maia <rennanmaia@gmail.com>
2022-11-04 15:15:45 -0300

    chore: add main files and project folders structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# espo
-In this project is shown development process of a outdoor advertising business 
+# ESPO - Empresa Santarena de Publicidade em Outdoor
+
+In this project is shown development process of a outdoor advertising business
+
+Here, we presentate a website construction to publish about advertising spaces and can book and manage that.
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ESPO - Empresa Santarena de publicidade em outdoor</title>
+</head>
+<body>
+  <header>
+      <div class="logo">Logo</div>
+      <div class="menu">Menu</div>
+  </header>
+
+  <content>
+    <div class="slider">Slider</div>
+    <div class="ads-list">ADS List</div>
+    <div class="testimonials">Depoimentos</div>
+    <div class="who-wer-are">Quem somos</div>
+  </content>
+
+  <footer>
+  </footer>
+</body>
+</html>
