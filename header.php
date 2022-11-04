cc5ecaf24986894fd8cb97909a22ef093b34f28e
Rennan Maia <rennanmaia@gmail.com>
2022-11-04 17:06:15 -0300

    style: add style, logo on page header

--- header.php
+++ header.php
@@ -1,4 +1,23 @@
-<header class="borda">
-      <div class="logo borda">Logo</div>
-      <div class="menu borda">Menu</div>
-  </header>
+<header>
+  <div class="logo">
+    <img src="images/logo-espo.png">
+  </div>
+  <button class="btn-menu">
+    <i class="fas fa-bars fa-lg"></i>
+  </button>
+
+  <div class="menu">
+    <a href="" class="btn-close">
+      <i class="fas fa-times fa-lg"></i>
+    </a>
+        <!-- botão para fechar o menu -->
+    <ul>
+      <li><a href="#" class="btn-menu-item">Home</a></li>
+      <li><a href="#" class="btn-menu-item">Destaques</a></li>
+      <li><a href="#" class="btn-menu-item">Tipos de serviço</a></li>
+      <li><a href="#" class="btn-menu-item">Espaços</a></li>
+      <li><a href="#" class="btn-menu-item">Depoimentos</a></li>
+      <li><a href="#" class="btn-menu-item">Quem somos</a></li>
+    </ul>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -5,21 +5,51 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ESPO - Empresa Santarena de publicidade em outdoor</title>
+  
+  <!-- Importação da fonte Lato do Google -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+      rel="stylesheet"
+    />
 
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <?php include_once 'header.php'; ?>
   
-
   <content>
-    <div class="slider borda">Slider</div>
-    <div class="ads-list borda">ADS List</div>
-    <div class="testimonials borda">Depoimentos</div>
-    <div class="who-wer-are borda">Quem somos</div>
+    <div class="slider border">Slider</div>
+    <?php include_once 'ads_list.php'; ?>
+    <div class="testimonials border">Depoimentos</div>
+    <div class="who-wer-are border">Quem somos</div>
   </content>
 
-  <footer class="borda">
+  <footer class="border">
   </footer>
 </body>
 </html>
+
+<script src="js/all.min.js"></script>
+<script src="js/jquery.js"></script>
+
+
+<script>
+  $('.btn-menu').click(function () {
+    $('.menu').show()
+  })
+
+  $('.btn-close').click(function () {
+    $('.menu').hide()
+    return false
+  })
+
+  // $('.btn-menu-item').click(function () {
+  //   $('.menu').hide()
+  //   return false
+  // })
+  $('.menu').click(function () {
+    $('.menu').hide()
+  })
+</script>
