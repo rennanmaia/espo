13fe8be3bddc22547ac4fea190aa0687273dab5a
Rennan Maia <rennanmaia@gmail.com>
2022-11-11 06:36:34 -0300

    feat: add a slider on the page and segment style and php file

---
+++ slider.php
@@ -0,0 +1,37 @@
+<div class="bxslider">
+    <div>
+      <img src="images/aeroporto-001.jpg"  title="Funky roots">
+
+      <div class="bx-caption">
+        <div class="title">Conheça nossos outdoors</div>
+        <div class="nc_subtitle">Estão posicionados nos melhores lugares da cidade</div>
+      </div>
+    </div>
+    <div>
+      <img src="images/aqui-cafe-001.jpg"  title="Funky roots">
+
+      <div class="bx-caption">
+        <div class="title">Ruas, praças, shoppings</div>
+        <div class="nc_subtitle">Escolha o melhor local para atingir seu público</div>
+      </div>
+    </div>
+    <div>
+      <img src="images/br-163-001.jpg"  title="Funky roots">
+
+      <div class="bx-caption">
+        <div class="title">Painéis de LED, placas diversas, outdoors de papel</div>
+        <div class="nc_subtitle">Escolha o formato de sua mídia e venda mais</div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    $('.bxslider').bxSlider({
+    auto: true,
+    autoControls: false,
+    stopAutoOnClick: true,
+    pager: true,
+    // slideWidth: 600,
+    captions: false
+  });
+  </script>  
